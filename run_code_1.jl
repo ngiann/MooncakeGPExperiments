c2396b183947f4638bae3a583c0425a0ce9b3894
Nikos Gianniotis <ngiann@gmail.com>
2026-03-03 10:22:23 +0100

    new file

---
+++ run_code_1.jl
@@ -0,0 +1,10 @@
+using BenchmarkTools
+using Distributions
+using DifferentiationInterface
+using LinearAlgebra
+using Optim
+using Random
+import Mooncake
+
+include("fitgp_1.jl")
+include("toydata.jl")
