60003fb9ac08ab06acaeb291ffae88b8c0fab896
Nikos Gianniotis <ngiann@gmail.com>
2026-03-03 10:22:14 +0100

    add comment

--- toydata.jl
+++ toydata.jl
@@ -1,5 +1,6 @@
 function toydata()
-
+    # generate some synthetic data, not important how exactly,
+    # just to have something to test the code on
     rng = MersenneTwister(1234)
     x = rand(rng, 1000)*10
     y = sin.(x) + 0.1*randn(rng, length(x))
