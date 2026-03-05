bb2a20ce9d550277b59cc54197638c17dcd427da
Nikos Gianniotis <ngiann@gmail.com>
2026-03-05 09:53:16 +0100

    update code

--- fitgp_1.jl
+++ fitgp_1.jl
@@ -3,13 +3,13 @@ function fitgp_1(t, x; iterations = 10, JITTER = 1e-6)
     # t are the target outputs (here one-dimensional)
     # x are the inputs (here one-dimensional)
 
-    # Initialise hyperparameters
+    # Initialise all hyperparameters to zero
     logℓ, logα, logσ = 0.0, 0.0, 0.0
    
     rbf(x, y, logℓ, logα) = exp(-abs2(x - y)*exp(logℓ))*exp(logα)
 
     function calculatecovariance!(K, logℓ, logα)
-        # Can be done better, but point here it to understand where
+        # Can be done better, but the point is to understand where
         # problems may arise with allocations and performance
         # when writing own code
 
@@ -28,14 +28,19 @@ function fitgp_1(t, x; iterations = 10, JITTER = 1e-6)
         return logℓ, logα, logσ
     end
 
+    # calculate the covariance matrix for the passed hyperparameters, add noise variance and jitter
+    function calculateK(x, logℓ, logα, logσ)
+        local K = zeros(length(x), length(x))
+        calculatecovariance!(K, logℓ, logα)
+        K += exp(logσ) * I # add noise variance to the diagonal
+        K += JITTER * I * exp(logα) # add jitter for numerical stability
+        return K
+    end
+
     function marginal_loglikelihood_MvNormal(logℓ, logα, logσ)
 
         # calculate the covariance matrix for the passed hyperparameters
-        local K = zeros(length(x), length(x))
-        calculatecovariance!(K, logℓ, logα)
-
-        # add noise variance to the diagonal and jitter for numerical stability   
-        K += exp(logσ) * I + JITTER * I 
+        local K = calculateK(x, logℓ, logα, logσ)
 
         # return log marginal likelihood of a Gaussian process for Gaussian likelihood
         return logpdf(MvNormal(zeros(length(x)), K), t)
@@ -46,15 +51,12 @@ function fitgp_1(t, x; iterations = 10, JITTER = 1e-6)
     function marginal_loglikelihood_explicit(logℓ, logα, logσ)
 
         # calculate the covariance matrix for the passed hyperparameters
-        local K = zeros(length(x), length(x))
-        calculatecovariance!(K, logℓ, logα)
-
-        # add noise variance to the diagonal and jitter for numerical stability   
-        K += exp(logσ) * I + JITTER * I 
+        local K = calculateK(x, logℓ, logα, logσ)
 
         # return log marginal likelihood of a Gaussian process for Gaussian likelihood
         local L = cholesky(Symmetric(K)).L
         return -0.5 * sum(abs2, L \ t) - sum(log, diag(L)) - 0.5*length(x)*log(2π)
+
     end
 
     # define negative log-likelihood for optimization
@@ -66,19 +68,28 @@ function fitgp_1(t, x; iterations = 10, JITTER = 1e-6)
     # differ only very slightly for the same hyperparameters.
     let
         initparam = randn(3)
-        @show nll_MvNormal(initparam)
-        @show nll_explicit(initparam)
+        @info "evaluate nll_MvNormal with random parameters" nll_MvNormal(initparam)
+        @info "evaluate nll_explicit with random parameters" nll_explicit(initparam)
     end
 
     # Optimise hyperparameters using LBFGS
     opt = Optim.Options(iterations = iterations, show_trace = true, show_every = 1)
 
     # optimise using the MvNormal implementation
-    optimize(nll_MvNormal, [logℓ, logα, logσ], LBFGS(), opt, autodiff=Mooncake.AutoMooncake())
-
-    # optimise using the explicit implementation
-    optimize(nll_explicit, [logℓ, logα, logσ], LBFGS(), opt, autodiff=Mooncake.AutoMooncake())
+    @info "optimising with nll_MvNormal"
+    res1 = optimize(nll_MvNormal, [logℓ, logα, logσ], LBFGS(), opt, autodiff=Mooncake.AutoMooncake())
+ 
+    # display condition number of the covariance matrix
+    @info "cond(K) after optimising with nll_MvNormal"
+    display(cond(calculateK(x, unpack_parameters(res1.minimizer)...)))
     
+    # optimise using the explicit implementation
+    @info "optimising with nll_explicit"
+    res2 = optimize(nll_explicit, [logℓ, logα, logσ], LBFGS(), opt, autodiff=Mooncake.AutoMooncake())
+
+    # display condition number of the covariance matrix
+    @info "cond(K) after optimising with nll_explicit"
+    display(cond(calculateK(x, unpack_parameters(res2.minimizer)...)))
 end
 
 
